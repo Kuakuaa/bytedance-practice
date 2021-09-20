347b3f325951ce79b7699c9d971b35ae26809393
Kuakuaa <1072063862@qq.com>
2021-09-20 21:15:25 +0800

    feat(restful): add home page and http request

--- day01/src/02_serve/restful/framework/router.js
+++ day01/src/02_serve/restful/framework/router.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const router = require('koa-router')();
 const { init, get, create, update, del, list } = require('./api');
 
@@ -6,5 +7,15 @@ router.get('/api/:list', init, list);
 router.post('/api/:list', init, create);
 router.put('/api/:list/:id', init, update);
 router.delete('/api/:list/:id', init, del);
+router.get('/', async (ctx,next) => {
+  ctx.response.type='text/html';
+  ctx.response.body = '<h1>Hello, Kuakuaa!</h1>';
+})
+router.get('/home', async (ctx,next) => {
+  const file = fs.readFileSync('./index.html');
+  ctx.response.type='text/html';
+  ctx.response.body = file;
+})
+
 
 module.exports = router.routes();
