63902d03645654097de18e304d2ead0c0d25803f
Kuakuaa <1072063862@qq.com>
2021-09-21 01:09:02 +0800

    revert(01_apis): addstr

--- day01/src/01_apis/03_http.js
+++ day01/src/01_apis/03_http.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const http = require('http');
 http.createServer((request, response) => {
   console.log('This is a request');
-  const {url, method} = request;
+  const { url, method, headers } = request;
   if (url === '/' && method === "GET") {
-    fs.readFile('./index.html',(err,data) => {
+    fs.readFile('./index.html', (err, data) => {
       if (err) {
         response.writeHead(500, {
           'Content-Type': 'text/plain;charset=utf-8'
@@ -16,6 +16,14 @@ http.createServer((request, response) => {
       response.setHeader('Content-Type', 'text/html');
       response.end(data);
     })
+  } else if (url === '/users' && method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ name: 'tom' }));
+  } else if (method === 'GET' && headers.accept.indexOf('image/*') !== -1) {
+    console.log("zhe");
+    // 直接用 readFile 读取 是否 ok 把全部图片内容加载到服务器
+    // stream 流 url /1.png => ./1.png
+    fs.createReadStream('.'+url).pipe(response);
   } else {
     response.statusCode = 404;
     response.setHeader('Content-Type', 'text/plain;charset=utf-8');
